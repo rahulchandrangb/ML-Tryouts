252c7449e859413cd7513d4adcc7fba3d3369f2d
Rahul Chandran <rahul845glndsmx@gmail.com>
2014-11-25 16:33:36 +0530

    Add recursive neural tensor network

---
+++ src/main/scala/rahul/NLP/RNTN.scala
@@ -0,0 +1,12 @@
+package rahul.NLP
+/*
+ * 
+ * Represents a Recursive Neural Tensor Network
+ * Details can be found at: http://nlp.stanford.edu/~socherr/EMNLP2013_RNTN.pdf
+ */
+class RNTN {
+
+}
+object RNTN{
+  
+}
