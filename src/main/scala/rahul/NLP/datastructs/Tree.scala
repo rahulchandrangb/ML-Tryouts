b5c736a69d1b2d6a370bee57b323aa1e7cff081b
Rahul Chandran <rahul845glndsmx@gmail.com>
2014-12-01 18:35:27 +0530

    Tree changes

--- src/main/scala/rahul/NLP/datastructs/Tree.scala
+++ src/main/scala/rahul/NLP/datastructs/Tree.scala
@@ -1,22 +1,31 @@
 package rahul.NLP.datastructs
 import breeze.linalg.{ DenseMatrix, DenseVector }
+import breeze.stats.distributions.Uniform
 import scala.collection.mutable.ListBuffer
 
-class Tree {
+class Tree(val leftChild: Tree,
+  val rightChild: Tree,
+  val value: DenseVector[Double],
+  val isLeaf: Boolean = false,
+  val hasSibling: Boolean = true) {
+
   val leafNodes = ListBuffer[(String, DenseVector[Double])]()
-  
   def insertWordVec(wvec: (String, DenseVector[Double])) {
-    leafNodes += wvec
+    if (isLeaf) leafNodes += wvec
+    else println("Error:Trying to insert leaf on an intermediate node.!!!")
   }
-  
-  def constructTree{
-    
-  } 
-  
-  def calculateScore={
-    
+
+  def constructTreeFromLeaf: Tree = {
+
+    return null
   }
-  def getParentVector={
-    
+
+  def calculateScore: Double = {
+    (new Uniform(0, 5)).draw //Later change it to score function
   }
+  def getParentVector(l: DenseVector[Double], r: DenseVector[Double]): DenseVector[Double] = {
+    //For now we'll give some vector , later we'll implement tanh 
+    DenseVector.rand(l.length)
+  }
+
 }
