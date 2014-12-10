923210e4537bb217cab461b8995ae8aeda4a872f
Rahul Chandran <rahul845glndsmx@gmail.com>
2014-12-10 09:21:42 +0530

    RNN test suite

---
+++ src/test/scala/rahul/nlp/RNNTestSuite.scala
@@ -0,0 +1,12 @@
+package rahul.nlp
+
+import org.scalatest._
+import scala.collection._
+
+class TreeTestSuite extends FlatSpec with Matchers {
+     "RNN Tree" should "" in {
+
+//     alignParser.alignSettings.getOrElse("headerSeen",0) should be (1)
+   }
+  
+}
