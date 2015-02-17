45c2689dab6df3a843998b4daf2dff6f273967ab
Rahul Chandran <rahul845glndsmx@gmail.com>
2015-02-17 11:46:21 +0530

    Monte Carlo test for integral added

--- src/main/scala/rahul/montecarlo/TestMonte.scala
+++ src/main/scala/rahul/montecarlo/TestMonte.scala
@@ -2,17 +2,16 @@ package rahul.montecarlo
 import breeze.stats.distributions.Uniform
 import scala.math._
 object TestMonte extends App{
-  
   println("Finding sinx integral from 0 to Pi/2 using Monte Carlo...\n-----------------------------------------------------------")
   def radianVal(x:Double) = 0.0174532925 * x //degree to radian
   def degreeVal(x:Double) = 57.2957795 * x //radian to degree
   val rng = new Uniform(0,1)
-  val testOut =   cos(0) - cos(Pi/2)
+  val testOut =   cos(0) - cos(Pi/4)
   val epochs  = 10000000
   val monteOut = (Iterator.iterate(0.0){
     case (s:Double) =>
-      val newNum = sin(rng.draw * Pi/2)
+      val newNum = sin(rng.draw * Pi/4)
       (s + newNum)
-  }.drop(epochs).next) * Pi * 0.5 / epochs 
+  }.drop(epochs).next) * Pi * 0.25 / epochs 
   println(s"\nDirectly calculated Integral Output : $testOut \nMonte-Carlo Calculated Output: $monteOut")
 }
