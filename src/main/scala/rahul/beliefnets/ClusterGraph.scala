a18a719d114ae3c1cc8e22fc0fcd3cb20638f485
Rahul Chandran <rahul845glndsmx@gmail.com>
2015-03-19 16:25:20 +0530

    changes in cluster graph

--- src/main/scala/rahul/beliefnets/ClusterGraph.scala
+++ src/main/scala/rahul/beliefnets/ClusterGraph.scala
@@ -14,9 +14,14 @@ class ClusterNode{
 /*
  *  1. Factors involved[eg: {A}, {A,B,C} etc]
  *  2. Connected neighbors 
- *  3. Received messages along with flag for condition of all messages received.
- *  4.  
+ *  3. Received messages [both directions] along with flag for condition of all messages received.
+ *  4. Send methods 
+ *  5. Properties [isLeaf,numNeighbors,] 
+ *  6. Consider reusing inference by implementing memoization
+ *  
  */  
+  
+  
 }
 
 
